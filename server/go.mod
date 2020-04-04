a127805e68e76cc7a07d41e7dbfcb8b06970a3b0
Fifciuu <filip.jdrasik@gmail.com>
2020-04-04 15:42:21 +0200

    Test endpoint

---
+++ server/go.mod
@@ -0,0 +1,14 @@
+module github.com/fifciu/what-can-i-do/server
+
+go 1.13
+
+require (
+	github.com/cosmtrek/air v1.12.1 // indirect
+	github.com/fatih/color v1.9.0 // indirect
+	github.com/gorilla/handlers v1.4.2
+	github.com/gorilla/mux v1.7.4
+	github.com/imdario/mergo v0.3.9 // indirect
+	github.com/mattn/go-colorable v0.1.6 // indirect
+	github.com/pelletier/go-toml v1.7.0 // indirect
+	golang.org/x/sys v0.0.0-20200331124033-c3d80250170d // indirect
+)
---
+++ server/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	controllers "github.com/fifciu/what-can-i-do/server/controllers"
+
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/",
+		controllers.HelloWorld).Methods("GET")
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8090"
+	}
+
+	fmt.Println("It works on http://localhost:" + port + "/ address")
+
+	allowedOrigins := handlers.AllowedOrigins([]string{"*"})
+	allowedHeaders := handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization"})
+	allowedMethods := handlers.AllowedMethods([]string{"POST", "GET"})
+
+	err := http.ListenAndServe(":"+port, handlers.CORS(allowedOrigins, allowedMethods, allowedHeaders)(router))
+	if err != nil {
+		fmt.Print(err)
+	}
+
+}
