14fe64c590aeb18a1c053e0658f9caded4bdb88a
Fifciuu <filip.jdrasik@gmail.com>
2020-04-19 12:39:19 +0200

    About page

---
+++ client/pages/about/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="about">
+    Hi!
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "index"
+    }
+</script>
+
+<style lang="scss" scoped>
+  .about {
+    padding: $cardPadding;
+  }
+</style>
