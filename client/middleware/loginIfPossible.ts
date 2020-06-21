b3e21033f9081916a61583ed60f52d3c2ab4ca23
Fifciuu <filip.jdrasik@gmail.com>
2020-06-21 16:09:43 +0200

    New Auth login

---
+++ client/middleware/loginIfPossible.ts
@@ -0,0 +1,32 @@
+import { Middleware } from '@nuxt/types'
+
+const loginIfPossibleMiddleware: Middleware = async ({ store, redirect }) => {
+  let token = store.state.auth.token
+  let expiresAt = store.state.auth.expiresAt
+
+  if (!store.state.auth.token || !store.state.auth.expiresAt) {
+    token = window.localStorage.getItem(<string>process.env.ls_token_key)
+    expiresAt = window.localStorage.getItem(<string>process.env.ls_expires_key)
+    await store.dispatch('auth/setCredentials', {
+      token,
+      expiresAt
+    })
+
+    if (!store.state.auth.user.email || !store.state.auth.user.name) {
+      // request to post /me
+      const success = await store.dispatch('auth/fetchAndSetUserdata', {
+        token
+      })
+      if (!success) {
+        // Token expired or forgery!
+        await store.dispatch('auth/logout', {
+          token
+        })
+      }
+    }
+  }
+}
+
+// TODO Only MyAccount tries to refresh token, apply it everywhere
+
+export default loginIfPossibleMiddleware
